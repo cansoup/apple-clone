6d6d2821c8c68201195629636633b05b38dfa7c9
gw2102961 <kimminha@gabia.com>
2021-12-09 08:43:39 +0900

    1209: scrollAction

--- apple-clone-v10/js/mainblank.js
+++ apple-clone-v10/js/mainblank.js
@@ -1,4 +1,9 @@
 (() => {
+
+  let yOffset = 0; // window.pageYOffset을 저장할 변수
+  let prevScrollHeight = 0; // 현재 스크롤 위치(yOffset)보다 이전에 위치한 스크를 섹션들의 스크롤 높이값의 합
+  let currentScene = 0; // 현재 활성화된(눈 앞에 보고있는) 씬(scroll-section)
+  
   const sceneInfo = [
     { 
       type: 'sticky',
@@ -40,11 +45,28 @@
       sceneInfo[i].scrollHeight = sceneInfo[i].heightNum * innerHeight;
       sceneInfo[i].objs.container.style.height = `${sceneInfo[i].scrollHeight}px`;
     }
-
-    console.log(sceneInfo);
   }
 
+  function scrollLoop() {
+    for( let i = 0; i < currentScene; i++) {
+      prevScrollHeight += sceneInfo[i].scrollHeight;
+    }
+
+    if( yOffset > prevScrollHeight + sceneInfo[currentScene].scrollHeight ) {
+      currentScene++;
+    }
+
+    if( yOffset < prevScrollHeight ) {
+      if(currentScene === 0) return;
+      currentScene--;
+    }
+  }
+  
   window.addEventListener('resize', setLayout());
+  window.addEventListener('scroll', () => {
+    yOffset = window.scrollY;
+    scrollLoop();
+  });
   
   setLayout();
 })();
